348324a34e24e0501591527c63a3a622a74021ad
Sònia Pardo <soniap.001@gmail.com>
2014-09-13 19:36:23 +0200

    arreglar menu arrel

--- helpers/link_helpers.rb
+++ helpers/link_helpers.rb
@@ -1,21 +1,40 @@
 module LinkHelpers
   def link_to_unless_current target
     trad = t("menu.#{target}")
-    if current_page.url =~ /\/#{target}/
-      "<a><span class='active'>#{trad}</span></a>"
-    else
-      "<a href='#{target}.html'><span>#{trad}</span></a>"
-    end
+    lang = I18n.locale
+    
+    link_to_with_lang "#{target}.html", trad, lang
   end
 
   def link_to_lang lang
-    target = "/#{current_page.path.split("/").last}"
+    target = remove_language_code
     trad = t "idioma.#{lang}"
+    
+    link_to_with_lang target, trad, lang
+  end
+
+  def lang_root_path
+    if I18n.default_locale == I18n.locale 
+      "/"
+    else
+      "/#{I18n.locale}/"
+    end
+  end
+
+  private
+
+  def link_to_with_lang target, trad, lang
     if lang == I18n.locale
       "<a><span class='active'>#{trad}</span></a>"
     else
       target = "/#{lang}#{target}" if lang != I18n.default_locale
       "<a href='#{target}'><span>#{trad}</span></a>"
     end
+  end
+
+  def remove_language_code
+    tmp = current_page.path.split("/")
+    tmp.shift if tmp.first =~ /^(es|ca)$/
+    "/#{tmp.join "/"}"
   end
 end
