4ef8e5ed6a94a4b3b91d2798300bd03440c8b5c9
Sònia Pardo <soniap.001@gmail.com>
2014-09-13 19:49:28 +0200

    moure espectacles a subcarpeta i arreglar enllaços

--- helpers/link_helpers.rb
+++ helpers/link_helpers.rb
@@ -2,15 +2,17 @@ module LinkHelpers
   def link_to_unless_current target
     trad = t("menu.#{target}")
     lang = I18n.locale
+    is_current = current_page.url =~ /\/#{target}/
     
-    link_to_with_lang "#{target}.html", trad, lang
+    link_to_with_lang "/#{target}.html", trad, lang, is_current
   end
 
   def link_to_lang lang
     target = remove_language_code
     trad = t "idioma.#{lang}"
+    is_current = lang == I18n.locale
     
-    link_to_with_lang target, trad, lang
+    link_to_with_lang target, trad, lang, is_current
   end
 
   def lang_root_path
@@ -23,8 +25,8 @@ module LinkHelpers
 
   private
 
-  def link_to_with_lang target, trad, lang
-    if lang == I18n.locale
+  def link_to_with_lang target, trad, lang, is_current
+    if is_current
       "<a><span class='active'>#{trad}</span></a>"
     else
       target = "/#{lang}#{target}" if lang != I18n.default_locale
