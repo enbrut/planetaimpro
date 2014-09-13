08d811bf9db9e275679dee49e4c98496861d5e93
1334 <1334@shadowproject.net>
2014-09-13 17:27:50 +0200

    activar menú canvi idioma

--- helpers/link_helpers.rb
+++ helpers/link_helpers.rb
@@ -7,4 +7,15 @@ module LinkHelpers
       "<a href='#{target}.html'><span>#{trad}</span></a>"
     end
   end
+
+  def link_to_lang lang
+    target = "/#{current_page.path.split("/").last}"
+    trad = t "idioma.#{lang}"
+    if lang == I18n.locale
+      "<a><span class='active'>#{trad}</span></a>"
+    else
+      target = "/#{lang}#{target}" if lang != I18n.default_locale
+      "<a href='#{target}'><span>#{trad}</span></a>"
+    end
+  end
 end
